bafbeea892c06cd45b5a049802a696057ccdd05f
Roberto <robertoyukio@outlook.com>
2022-03-02 23:24:41 -0300

    Global styles and CSS variables created.Header component created and styled.

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header-container">
+      <h1 className="title">Qual o número?</h1>
+      <div className="separator"></div>
+
+    </header>
+  );
+}
+
+export default Header;
